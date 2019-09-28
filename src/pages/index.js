f5ad522eaab20b7bef83a33383622dfb70b65b26
= <jmarie@bpimedia.com>
2019-09-28 13:19:19 -0400

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,47 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export default ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello</h1>
-    <p> I'm a Denver native living and working in Washington, DC. I work on the Labs Team at <a href="https://bpimedia.com">Bully Pulpit Interactive,</a> where I'm building products at the intersection of politics, communications, data, and technology.</p>
-    <h1>Social</h1>
-    <p>You can find me online on <a href="https://twitter.com/joestemarie">Twitter</a> and <a href="https://www.linkedin.com/in/joestemarie/">LinkedIn</a>.</p>
-    <h1>Writing</h1>
-    <p>I really enjoy writing and find that it helps me focus my thinking.</p>
-    <ul>
-      {data.allMarkdownRemark.edges.map(({ node }) => (
-        <li><a href={node.fields.slug}><b>{node.frontmatter.title}.</b> ({node.frontmatter.date}).</a> {node.frontmatter.excerpt}</li>
-      ))}
-    </ul>
-  </Layout>
-)
+function dateSort(a, b) {
+  // sort posts by date
+  const dateA = a.node.frontmatter.sortDate;
+  const dateB = b.node.frontmatter.sortDate;
+
+  let comparison = 0;
+  if (dateA < dateB) {
+    comparison = 1;
+  } else if (dateA > dateB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+export default function ({ data }) {
+  // convert other posts into consistent format with markdown
+  var otherPosts = data.allOtherPostsYaml.nodes.map((post) => {
+    return({node: {
+      fields: {slug: post.URL},
+      frontmatter: {title: post.Title, date: post.Date, excerpt: post.Excerpt, sortDate: post.sortDate}
+    }})
+  })
+  var allPosts = [...otherPosts, ...data.allMarkdownRemark.edges].sort(dateSort);
+  console.log(allPosts);
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hello</h1>
+      <p>I'm a Denver native living and working in Washington, DC. I work on the Labs Team at <a href="https://bpimedia.com">Bully Pulpit Interactive,</a> where I'm building products at the intersection of politics, communications, data, and technology.</p>
+      <h1>Social</h1>
+      <p>You can find me online on <a href="https://twitter.com/joestemarie">Twitter</a> and <a href="https://www.linkedin.com/in/joestemarie/">LinkedIn</a>.</p>
+      <h1>Writing</h1>
+      <p>I really enjoy writing and find that it helps me focus my thinking.</p>
+      <ul>
+        {allPosts.map(({ node }) => (
+          <li><a href={node.fields.slug}><b>{node.frontmatter.title}.</b> ({node.frontmatter.date}).</a> {node.frontmatter.excerpt}</li>
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export const query = graphql `
   query MyQuery {
@@ -32,12 +57,22 @@ export const query = graphql `
           frontmatter {
             title
             excerpt
-            date(formatString: "Do MMMM YYYY")
+            date(formatString: "D MMMM YYYY")
+            sortDate: date
           }
           fields {
             slug
           }
         }
+      }
+    }
+    allOtherPostsYaml {
+      nodes {
+        URL
+        Title
+        Date(formatString: "D MMM YYYY")
+        sortDate: Date
+        Excerpt
       }
     }
   }
--- src/posts/2019-09-serverless.md
+++ src/posts/2019-09-serverless.md
@@ -1,6 +1,18 @@
 ---
 title: The serverless, low-code future
-date: 2019-09-15
+date: 2019-09-28
+mode: draft
 excerpt: Reflections on how AWS Lambda, Netlify, and the like are changing the way we build apps.
 ---
-An initial post.
+
+When I started to learn how to build apps a few years ago, before I could build any kind of meaningful backend, I needed to learn about server configuration. I had to understand relational databases. I had to understand how to set up a background task server. I had to learn how to set up load balancers in AWS. There are _much_ harder architectural patterns than what I built at that time, but it took a lot of time and patience to learn those things.
+
+When I built this blog, I didn't have to think about the servers it was going to run on. All I did was write the code, upload it to the cloud, and behold: a fully functioning site.
+
+It's important for people beyond developers to understand how this changes the game.
+
+[Paul Johnston is really smart on this:](https://twitter.com/PaulDJohnston/status/1073348296066912259) "Serverless is about understanding when *not* to create technology."
+
+But all of that is actually really irrelevant to the task at hand: creating an app to solve a problem. By abstracting away a lot of the configuration and infrastructure work, I can focus on the questions that are really critical: how do I make this do something really powerful? How do I make this really easy for people to use?
+
+That change will seriously decrease the barrier to entry to build apps. That's a great thing - more people can build powerful tech that helps them take on bigger problems.
